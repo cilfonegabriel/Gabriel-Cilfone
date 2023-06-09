8095cb7b8926124ee5aa2b32f6bcff84e69e5e6e
gabicilfone <cilfonegabriel@gmail.com>
2023-06-09 18:19:02 -0300

    fix linters

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
@@ -49,7 +51,7 @@ const ProjectCard = ({
 
         <div className='mt-5'>
           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          <p className='mt-2 text-secondary text-[14px] h-[200px]'>{description}</p>
         </div>
 
         <div className='mt-4 flex flex-wrap gap-2'>
